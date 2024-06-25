987a696fc2992f9fb31e76512ba9414b1daad17a
omar Amer <omar@gmail.com>
2024-06-26 01:09:18 +0300

    add assets and styles

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,19 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_dashboard/views/adaptive_layout_widget.dart';
+import 'package:flutter_responsive_dashboard/views/widgets/desctop_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+          mobileLayout: (context) => const Placeholder(),
+          tabletLayout: (context) => const Placeholder(),
+          desktopLayout: (context) => const DesctopLayout()),
+    );
   }
 }
---
+++ lib/views/widgets/desctop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DesctopLayout extends StatelessWidget {
+  const DesctopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
