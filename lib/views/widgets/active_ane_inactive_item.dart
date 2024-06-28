8d938a2d6a51c1ae0ba3006e7f5d92d12bd6fff5
omar Amer <omar@gmail.com>
2024-06-28 23:01:25 +0300

    handel active and inactive darwer items

---
+++ lib/views/widgets/active_ane_inactive_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_dashboard/model/listile_model.dart';
+import 'package:flutter_responsive_dashboard/utils/app_styles.dart';
+import 'package:flutter_svg/svg.dart';
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({
+    super.key,
+    required this.listileModel,
+  });
+
+  final ListileModel listileModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(listileModel.image),
+      title: Text(
+        listileModel.title,
+        style: AppStyles.styleRegular16(context),
+      ),
+    );
+  }
+}
+
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({
+    super.key,
+    required this.listileModel,
+  });
+
+  final ListileModel listileModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(listileModel.image),
+      title: Text(
+        listileModel.title,
+        style: AppStyles.styleBold16(context),
+      ),
+      trailing: Container(
+        width: 3.27,
+        decoration: const BoxDecoration(color: Color(0xff4EB7F2)),
+      ),
+    );
+  }
+}
--- lib/views/widgets/custom_drawer_listile.dart
+++ lib/views/widgets/custom_drawer_listile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive_dashboard/model/listile_model.dart';
-import 'package:flutter_responsive_dashboard/utils/app_styles.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
+import 'active_ane_inactive_item.dart';
 
 class CustomDrawerListile extends StatelessWidget {
   const CustomDrawerListile(
@@ -13,49 +13,5 @@ class CustomDrawerListile extends StatelessWidget {
     return isActive
         ? ActiveDrawerItem(listileModel: listileModel)
         : InActiveDrawerItem(listileModel: listileModel);
-  }
-}
-
-class InActiveDrawerItem extends StatelessWidget {
-  const InActiveDrawerItem({
-    super.key,
-    required this.listileModel,
-  });
-
-  final ListileModel listileModel;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(listileModel.image),
-      title: Text(
-        listileModel.title,
-        style: AppStyles.styleRegular16(context),
-      ),
-    );
-  }
-}
-
-class ActiveDrawerItem extends StatelessWidget {
-  const ActiveDrawerItem({
-    super.key,
-    required this.listileModel,
-  });
-
-  final ListileModel listileModel;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(listileModel.image),
-      title: Text(
-        listileModel.title,
-        style: AppStyles.styleBold16(context),
-      ),
-      trailing: Container(
-        width: 3.27,
-        decoration: const BoxDecoration(color: Color(0xff4EB7F2)),
-      ),
-    );
   }
 }
